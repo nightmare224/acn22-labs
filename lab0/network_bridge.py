1a6fe661c63dd5358b1c1af8ca985de37a6180bd
Nicole Tai <h.tai2@student.vu.nl>
2022-11-03 19:40:18 +0100

    lab0 done

--- lab0/network_bridge.py
+++ lab0/network_bridge.py
@@ -26,7 +26,21 @@ class BridgeTopo(Topo):
 
         Topo.__init__(self)
 
-        # TODO: add nodes and links to construct the topology
+    def build(self):
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
 
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+        self.addLink(s1, s2)
 
 topos = {'bridge': (lambda: BridgeTopo())}
+# enter mn with custom topology
+# sudo mn --custom network_bridge.py --topo=bridge --link=tc
