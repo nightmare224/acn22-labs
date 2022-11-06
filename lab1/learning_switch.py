3d70f67d56b3b0953c7c62f07fd69fe416314ca0
Nicole Tai <nicole1999224@gmail.com>
2022-11-06 23:35:59 +0100

    lab1 worked, but the way to check whether is arp packet or not seems to be not elegant

--- lab1/learning_switch.py
+++ lab1/learning_switch.py
@@ -19,8 +19,8 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import arp
+# from ryu.lib.packet import ipv4
+# from ryu.lib.packet import arp
 
 class LearningSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -55,25 +55,62 @@ class LearningSwitch(app_manager.RyuApp):
                                 match=match, instructions=inst)
         datapath.send_msg(mod)
 
+    def is_arp(self, protocols):
+        for p in protocols:
+            if hasattr(p, 'protocol_name'):
+                # print(p.protocol_name)
+                if 'arp' == p.protocol_name:
+                    return True
+        return False
+
     # Handle the packet_in event
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         
         msg = ev.msg
+        # print(msg)
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # Get datapath ID to identify the switch
+
+        # init mac_to_port
+        self.mac_to_port.setdefault(datapath.id, {})
+
+        # store path in to table
+        pkt = packet.Packet(msg.data)
+
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        mac_src = eth.src
+        mac_dst = eth.dst
         dpid = datapath.id
+        in_port = msg.match['in_port']
+        if not self.is_arp(pkt.protocols):
+            # update in mac table if it is not related to arp
+            self.mac_to_port[dpid][mac_src] = in_port
 
-        # TODO: learning switch implementation
+        # if not found then broadcast to every port
+        if mac_dst not in self.mac_to_port[dpid]:
+            out_port = ofproto.OFPP_FLOOD
+        else:
+            out_port = self.mac_to_port[dpid][mac_dst]
+        # print(self.mac_to_port)
+        actions = [parser.OFPActionOutput(out_port)]
 
+        # install flow (why need this part??)
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port = in_port, eth_dst = mac_dst)
+            # match = parser.OFPMatch(in_port = in_port, eth_dst = mac_dst, eth_src = mac_src)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
 
         # Construct packet_out message and send it
         out = parser.OFPPacketOut(datapath=datapath,
                                   in_port=in_port, 
                                   actions=actions, 
-                                  buffer_id=datapath.ofproto.OFP_NO_BUFFER,
+                                  buffer_id=msg.buffer_id, # datapath.ofproto.OFP_NO_BUFFER
                                   data=msg.data)
         datapath.send_msg(out)
