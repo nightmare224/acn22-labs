c4cf9aab2698c613534f2ef312236300cbef7867
yssamtu <yssamtu@gmail.com>
2022-12-17 19:43:07 +0100

    store result

--- lab5/sml-udp/worker.py
+++ lab5/sml-udp/worker.py
@@ -1,6 +1,9 @@
 from scapy.all import get_if_hwaddr
 from scapy.all import Packet
 from scapy.config import conf
+from scapy.data import ETH_P_ARP
+from scapy.data import ETH_P_IP
+from scapy.data import IP_PROTOS
 from scapy.fields import ByteField
 from scapy.layers.inet import IP
 from scapy.layers.inet import UDP
@@ -8,10 +11,15 @@ from scapy.layers.l2 import ARP
 from scapy.layers.l2 import Ether
 from scapy.packet import Raw
 from socket import AF_INET
+from socket import AF_PACKET
+from socket import htons
 from socket import inet_ntop
 from socket import SOCK_DGRAM
+from socket import SOCK_RAW
 from socket import socket
 from struct import pack
+from lib.comm import send
+from lib.comm import receive
 from lib.gen import GenInts
 from lib.gen import GenMultipleOfInRange
 from lib.test import CreateTestData
@@ -20,6 +28,11 @@ from lib.worker import GetRankOrExit
 from lib.worker import ip
 from lib.worker import Log
 from config import NUM_WORKERS
+from struct import iter_unpack
+from scapy.data import ETHER_BROADCAST
+from scapy.layers.l2 import DestMACField
+from binascii import hexlify
+# print(hexlify(ETHER_BROADCAST, ":"))
 
 NUM_ITER = 1     # TODO: Make sure your program can handle larger values
 CHUNK_SIZE = 3  # TODO: Define me
@@ -31,16 +44,13 @@ for route in conf.route.routes:
     if SRC_IP_ADDR in route:
         DST_IP_ADDR = inet_ntop(AF_INET, pack("!I", route[0]))
         break
-# print(conf.route)
 SRC_PORT = 38787
 DST_PORT = 38788
-ETH_TYPE = 0x0806
 IP_HEADER_LEN = 20
 UDP_HEADER_LEN = 8
 SWITCH_ML_HEADER_LEN = 2
 UDP_TOTAL_LEN = UDP_HEADER_LEN + SWITCH_ML_HEADER_LEN + CHUNK_SIZE * 4
 IP_TOTAL_LEN = IP_HEADER_LEN + UDP_TOTAL_LEN
-IP_PROTO = 0x11
 
 
 class SwitchML(Packet):
@@ -63,40 +73,46 @@ def AllReduce(soc, rank, data, result):
 
     This function is blocking, i.e. only returns with a result or error
     """
-    # for i in range(len(data) // CHUNK_SIZE):
+    global DST_MAC_ADDR
     if DST_MAC_ADDR == "ff:ff:ff:ff:ff:ff":
         pkt_snd = (
-            Ether(dst=DST_MAC_ADDR, src=SRC_MAC_ADDR, type=ETH_TYPE) /
-            ARP(hwtype=1, ptype=0x0800, hwlen=6, plen=4, op=1, hwsrc=SRC_MAC_ADDR, psrc=SRC_IP_ADDR, pdst=DST_IP_ADDR)
+            Ether(dst=DST_MAC_ADDR, src=SRC_MAC_ADDR, type=ETH_P_ARP) /
+            ARP(hwtype=1, ptype=0x0800, hwlen=6, plen=4, op=1,
+                hwsrc=SRC_MAC_ADDR, psrc=SRC_IP_ADDR, pdst=DST_IP_ADDR)
         ).build()
-        print(Ether(pkt_snd).display())
-        # s = socket()
-    for i in range(2):
+        s = socket(family=AF_PACKET, type=SOCK_RAW, proto=htons(ETH_P_ARP))
+        s.bind(("eth0", ETH_P_ARP))
+        s.send(pkt_snd)
+        DST_MAC_ADDR = ARP(Ether(s.recv(len(pkt_snd))).payload).hwsrc
+        s.close()
+
+    for i in range(len(data) // CHUNK_SIZE):
+    # for i in range(2):
         payload = bytearray()
-        # for num in data[CHUNK_SIZE*i:CHUNK_SIZE*(i+1)]:
-        for num in [1, 1, 1]:
+        for num in data[CHUNK_SIZE*i:CHUNK_SIZE*(i+1)]:
+        # for num in [1, 2, 3]:
             payload.extend(pack("!I", num))
         pkt_snd = (
-            Ether(dst=DST_MAC_ADDR, src=SRC_MAC_ADDR, type=ETH_TYPE) /
-            IP(ihl=5, len=IP_TOTAL_LEN, id=i, proto=IP_PROTO, src=SRC_IP_ADDR, dst=DST_IP_ADDR) /
+            Ether(dst=DST_MAC_ADDR, src=SRC_MAC_ADDR, type=ETH_P_IP) /
+            IP(ihl=5, len=IP_TOTAL_LEN, id=i, proto=IP_PROTOS.udp, src=SRC_IP_ADDR, dst=DST_IP_ADDR) /
             UDP(sport=SRC_PORT, dport=DST_PORT, len=UDP_TOTAL_LEN) /
             SwitchML(rank=rank, num_workers=NUM_WORKERS) /
             Raw(payload)
         ).build()
-        # print(Ether(pkt_snd).display())
-        soc.sendto(pkt_snd, (DST_IP_ADDR, DST_PORT))
-        # tmp = soc.recvfrom(1024)
+        send(soc, pkt_snd, (DST_IP_ADDR, DST_PORT))
+        pkt_recv = receive(soc, len(pkt_snd))
+        byte_data = SwitchML(UDP(IP(Ether(pkt_recv).payload).payload).payload).payload.load
+        for j, num in enumerate(iter_unpack("!I", byte_data)):
+            result[i * CHUNK_SIZE + j] = num[0]
     # NOTE: Do not send/recv directly to/from the socket.
     #       Instead, please use the functions send() and receive() from lib/comm.py
     #       We will use modified versions of these functions to test your program
-    pass
 
 
 def main():
     rank = GetRankOrExit()
 
     s = socket(family=AF_INET, type=SOCK_DGRAM)
-    # s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     # NOTE: This socket will be used for all AllReduce calls.
     #       Feel free to go with a different design (e.g. multiple sockets)
     #       if you want to, but make sure the loop below still works
